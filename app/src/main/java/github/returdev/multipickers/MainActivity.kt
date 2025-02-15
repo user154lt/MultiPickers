c0acf59cb1115963ff45a73bc888a45ba52df043
User154lt <63184070+user154lt@users.noreply.github.com>
2025-02-15 16:42:44 +0000

    Show previews in MainActivity.kt

--- app/src/main/java/github/returdev/multipickers/MainActivity.kt
+++ app/src/main/java/github/returdev/multipickers/MainActivity.kt
@@ -3,13 +3,19 @@ package github.returdev.multipickers
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TimePicker
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import github.returdev.multipickers.ui.theme.MultipickersTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,16 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column(
+                        modifier = Modifier.fillMaxSize()
+                            .padding(12.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.spacedBy(12.dp)
+                    ) {
+                        TextPickerPreview()
+                        NumberPickerPreview()
+                        PickerColorPossibilityPreview()
+                    }
                 }
             }
         }
