d5313a59ad1e1b440fc5bc3263dbdd46f841d18d
User154lt <63184070+user154lt@users.noreply.github.com>
2025-02-15 21:03:12 +0000

    Initial migration to multiplatform

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,36 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.kotlin.plugin.compose)
+    alias(libs.plugins.jetbrains.compose)
 }
+
+kotlin {
+    androidTarget {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_1_8)
+        }
+    }
+
+    sourceSets {
+        val androidMain by getting {
+            dependencies {
+                implementation("androidx.core:core-ktx:1.12.0")
+                implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+                implementation("androidx.activity:activity-compose:1.8.2")
+                implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+                implementation("androidx.compose.ui:ui")
+                implementation("androidx.compose.ui:ui-graphics")
+                implementation("androidx.compose.ui:ui-tooling-preview")
+                implementation("androidx.compose.material3:material3")
+                implementation(project(":multipickers"))
+            }
+        }
+    }
+}
+
 
 android {
     namespace = "github.returdev.multipickers"
@@ -33,9 +62,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+/*
     kotlinOptions {
         jvmTarget = "1.8"
     }
+*/
     buildFeatures {
         compose = true
     }
@@ -50,16 +81,6 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    implementation(project(":multipickers"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
